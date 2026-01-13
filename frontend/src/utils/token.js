6bb7dbbee1e0389ed300a472cf717bf02f67280a
Ram Krishna <krishnarammhd@gmail.com>
2026-01-13 15:45:51 +0530

    Fixed the issue of not logout automatically on the next visit to the website

--- frontend/src/utils/token.js
+++ frontend/src/utils/token.js
@@ -1,5 +1,5 @@
 const TOKEN_KEY = "auth_token";
 
-export const getToken = () => localStorage.getItem(TOKEN_KEY);
-export const setToken = (token) => localStorage.setItem(TOKEN_KEY, token);
-export const removeToken = () => localStorage.removeItem(TOKEN_KEY);
+export const getToken = () => sessionStorage.getItem(TOKEN_KEY);
+export const setToken = (token) => sessionStorage.setItem(TOKEN_KEY, token);
+export const removeToken = () => sessionStorage.removeItem(TOKEN_KEY);
